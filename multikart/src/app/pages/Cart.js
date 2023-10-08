797b1500376c9f1b22b555855baeeedf6c840edb
RNS0405 <rushikeshsonawane0405@gmail.com>
2023-10-08 20:30:28 +0530

    Cart file added

--- multikart/src/app/pages/Cart.js
+++ multikart/src/app/pages/Cart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "../../assets/css/cart.css";
 
 function Cart() {
@@ -7,19 +7,19 @@ function Cart() {
     <div>
       <div className="containerfluid bg-cart">
         <div className="row ">
-          <div className="col-lg-6 offset-1">
+          <div className="col-lg-6">
             <h2 className="cart_header_heading">CART</h2>
           </div>
           <div className="col-lg-2  offset-2">
             <ul className="d-flex list-unstyled">
               <li className="ms-2">
-                <Link className="text-decoration-none list_heading" to="/">
+                <a className="text-decoration-none list_heading" to="/">
                   HOME
-                </Link>
+                </a>
                 <span className="fs-5 ps-2 list_heading ">/</span>
               </li>
               <li className="ms-2">
-                <Link className="text-decoration-none list_heading">CART</Link>
+                <a className="text-decoration-none list_heading">CART</a>
               </li>
             </ul>
           </div>
@@ -28,9 +28,9 @@ function Cart() {
 
       <div className="container">
           <div className="row">
-            <div className="col-12 text-center mt-5">
-              <h3><strong>Your Cart is Empty</strong></h3>
-              <h4>Explore More Sortlist Some Items</h4>
+            <div className="col-12 ">
+              <p>Your Cart is Empty</p>
+              <p>Explore More Sortlist Some Items</p>
             </div>
           </div>
         </div>
