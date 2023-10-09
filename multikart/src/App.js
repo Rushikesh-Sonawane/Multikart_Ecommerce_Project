05f268674675e2185284610a343a3dc2b2c38a75
vaibhavaldar <vaibhavaldar0@gmail.com>
2023-10-09 09:22:43 +0530

    new files added by vaibhav

--- multikart/src/App.js
+++ multikart/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cart from './app/pages/Cart';
+import Wishlist from './app/pages/Wishlist';
+import Checkout from './app/pages/Checkout';
 
 
 
@@ -9,6 +11,8 @@ function App() {
   return (
     <BrowserRouter>
     <Cart/>
+    <Wishlist />
+    <Checkout />
     </BrowserRouter>
     
   );
